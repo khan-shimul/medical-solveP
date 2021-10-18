48f7cd83894333ba9a1e943f20afa39f462dcfcf
shimulkhan <khanshimul196@gmail.com>
2021-10-18 15:06:13 +0600

    update customer service

--- src/components/Home/CustomerService/CustomerService.js
+++ src/components/Home/CustomerService/CustomerService.js
@@ -4,43 +4,45 @@ import './CustomerService.css';
 
 const CustomerService = () => {
     return (
-        <div className="container my-5">
-            <Row>
-                <div className="col-12 col-md-4 d-flex align-items-center ">
-                    <div className="bg-icon d-flex align-items-center justify-content-center">
+        <section className="customer-service my-5 py-5 bg-secondary bg-opacity-10">
+            <div className="container">
+                <Row>
+                    <div className="col-12 col-md-4 d-flex align-items-center ">
+                        <div className="bg-icon d-flex align-items-center justify-content-center">
 
-                        <i className="fas fa-plane fs-2 text-white"></i>
+                            <i className="fas fa-plane fs-2 text-white"></i>
 
+                        </div>
+                        <div>
+                            <h5>FREE SHIPPING WORLDWIDE</h5>
+                            <p>On order over $99 - 7 days a week</p>
+                        </div>
                     </div>
-                    <div>
-                        <h5>FREE SHIPPING WORLDWIDE</h5>
-                        <p>On order over $99 - 7 days a week</p>
-                    </div>
-                </div>
-                <div className="col-12 col-md-4 d-flex align-items-center ">
-                    <div className="bg-icon d-flex align-items-center justify-content-center">
+                    <div className="col-12 col-md-4 d-flex align-items-center ">
+                        <div className="bg-icon d-flex align-items-center justify-content-center">
 
-                        <i className="fas fa-truck fs-2 text-white"></i>
+                            <i className="fas fa-truck fs-2 text-white"></i>
 
+                        </div>
+                        <div>
+                            <h5>CASH ON DELIVERY</h5>
+                            <p>100% money back guarantee</p>
+                        </div>
                     </div>
-                    <div>
-                        <h5>CASH ON DELIVERY</h5>
-                        <p>100% money back guarantee</p>
-                    </div>
-                </div>
-                <div className="col-12 col-md-4 d-flex align-items-center ">
-                    <div className="bg-icon d-flex align-items-center justify-content-center">
+                    <div className="col-12 col-md-4 d-flex align-items-center ">
+                        <div className="bg-icon d-flex align-items-center justify-content-center">
 
-                        <i className="fas fa-headset fs-2 text-white"></i>
+                            <i className="fas fa-headset fs-2 text-white"></i>
 
+                        </div>
+                        <div>
+                            <h5>24/7 CUSTOMER SERVICE</h5>
+                            <p>Call us 24/7 at 123-456-789</p>
+                        </div>
                     </div>
-                    <div>
-                        <h5>24/7 CUSTOMER SERVICE</h5>
-                        <p>Call us 24/7 at 123-456-789</p>
-                    </div>
-                </div>
-            </Row>
-        </div>
+                </Row>
+            </div>
+        </section>
     );
 };
 
