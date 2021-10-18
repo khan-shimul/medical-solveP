37d4babae66b5f618da9bfa9d7123293d089060c
shimulkhan <khanshimul196@gmail.com>
2021-10-18 14:48:42 +0600

    customer service section added

---
+++ src/components/Home/CustomerService/CustomerService.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Row } from 'react-bootstrap';
+import './CustomerService.css';
+
+const CustomerService = () => {
+    return (
+        <div className="container my-5">
+            <Row>
+                <div className="col-12 col-md-4 d-flex align-items-center ">
+                    <div className="bg-icon d-flex align-items-center justify-content-center">
+
+                        <i className="fas fa-plane fs-2 text-white"></i>
+
+                    </div>
+                    <div>
+                        <h5>FREE SHIPPING WORLDWIDE</h5>
+                        <p>On order over $99 - 7 days a week</p>
+                    </div>
+                </div>
+                <div className="col-12 col-md-4 d-flex align-items-center ">
+                    <div className="bg-icon d-flex align-items-center justify-content-center">
+
+                        <i className="fas fa-truck fs-2 text-white"></i>
+
+                    </div>
+                    <div>
+                        <h5>CASH ON DELIVERY</h5>
+                        <p>100% money back guarantee</p>
+                    </div>
+                </div>
+                <div className="col-12 col-md-4 d-flex align-items-center ">
+                    <div className="bg-icon d-flex align-items-center justify-content-center">
+
+                        <i className="fas fa-headset fs-2 text-white"></i>
+
+                    </div>
+                    <div>
+                        <h5>24/7 CUSTOMER SERVICE</h5>
+                        <p>Call us 24/7 at 123-456-789</p>
+                    </div>
+                </div>
+            </Row>
+        </div>
+    );
+};
+
+export default CustomerService;
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import CustomerService from '../CustomerService/CustomerService';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <CustomerService />
         </div>
     );
 };
