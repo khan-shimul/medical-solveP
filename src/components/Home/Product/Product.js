bdd9027cdb8495eefc263e9c7d083b07beb11827
shimulkhan <khanshimul196@gmail.com>
2021-10-18 20:03:51 +0600

    update product card

--- src/components/Home/Product/Product.js
+++ src/components/Home/Product/Product.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
+import Rating from 'react-rating';
 import './Product.css';
 
 
 const Product = ({ product }) => {
+    // destructure product info
     const { name, img, description, price, rating } = product;
 
     return (
         <Col>
-            <Card className="food text-center p-2 border-0 meal">
+            <Card className="product h-100 p-2">
                 <Card.Img variant="top" src={img} />
                 <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                        {description.slice(0, 25)}...
+                    <Card.Title className="fs-5">{name}</Card.Title>
+                    <Card.Text className="fs-6">
+                        {description.slice(0, 30)}...
                     </Card.Text>
-                    <h5>${price}</h5>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <Rating className="icons"
+                                initialRating={rating}
+                                emptySymbol="far fa-star icon-color"
+                                fullSymbol="fas fa-star icon-color"
+                                readonly
+                            />
+                        </div>
+                        <div className="col-md-6">
+                            <Card.Text className="price">${price}</Card.Text>
+                        </div>
+                    </div>
+                    <button className="btn-regular">Details</button>
                 </Card.Body>
             </Card>
         </Col>
--- src/components/Home/Products/Products.js
+++ src/components/Home/Products/Products.js
@@ -5,25 +5,31 @@ import Product from '../Product/Product';
 import './Products.css';
 
 const Products = () => {
+    // use products
     const [products] = useAuth();
+
     return (
-        <div>
+        <section>
+            {/* Products Intro */}
             <div className="products-intro text-center">
                 <h2>High Demand Products</h2>
                 <p>Medical devices and health care products</p>
                 <div className="line d-flex mx-auto"></div>
             </div>
-            <Container>
-                <Row xs={1} sm={2} md={4} className="g-4">
-                    {
-                        products.map(product => <Product
-                            key={product.id}
-                            product={product}
-                        ></Product>)
-                    }
-                </Row>
-            </Container>
-        </div>
+            {/* Products Container */}
+            <div className="mt-4">
+                <Container>
+                    <Row xs={1} sm={2} md={4} className="g-4">
+                        {
+                            products.map(product => <Product
+                                key={product.id}
+                                product={product}
+                            ></Product>)
+                        }
+                    </Row>
+                </Container>
+            </div>
+        </section>
     );
 };
 
